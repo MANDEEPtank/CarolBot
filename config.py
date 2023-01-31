0ec9168c44f2d3d0bc98452cebf47b91e735d9fe
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-01-31 09:29:36 -0600

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5530836902:AAGE1BCN25bUwnuANtmA-1SHI-_pFqzrju8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2072733473:AAHKmFKkRyKmjYrlPQurTXm0NOm47_FkTaA")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "5291894"))
@@ -19,16 +19,16 @@ APP_ID = int(os.environ.get("APP_ID", "5291894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5a64cb8bd39b2be463d0314295f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001688920583"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001666373414"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1132901778"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1018100459"))
 
 #Port
-PORT = os.environ.get("PORT", "8081")
+PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://server04-1:server04-1@cluster0.pqtpjic.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Carol_ibot:Carol_ibot@cluster0.16trvs1.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
@@ -37,10 +37,10 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first} \n\nCurrently I'm Working Only For My Owners.</b>")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1132901778").split()):
+    for x in (os.environ.get("ADMINS", "1018100459").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previouscaption}\nJOIN 💎 : @MINIFLIX</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
